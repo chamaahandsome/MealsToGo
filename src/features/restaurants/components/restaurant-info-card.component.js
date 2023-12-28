fa0587f3a4079d9289fc58deacbcea0a5479541f
chamaahandsome <timboywa@gmail.com>
2023-12-28 06:03:56 -0500

    created svg and built a spacer component

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,12 @@
 import React from "react";
 import styled from "styled-components/native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import { Text, Image, View } from "react-native";
+
+import { Spacer } from "../../../components/spacer/spacer.component";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
   backgroundcolor: ${(props) => props.theme.colors.bg.primary};
@@ -11,16 +17,46 @@ const RestaurantCardCover = styled(Card.Cover)`
   backgroundcolor: ${(props) => props.theme.colors.bg.primary};
 `;
 
-const Title = styled.Text`
+const Address = styled.Text`
   font-family: ${(props) => props.theme.fonts.body};
-  padding: ${(props) => props.theme.space[3]};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Title = styled.Text`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-bottom: ${(props) => props.theme.space[1]};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+// const Open = styled(SvgXml)`
+//   flex-direction: row;
+// `;ssssssssssssss
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Kibandaski",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.simplybeefandlamb.co.uk/media/1448/authentic-beef-curry-web_1.jpg?width=1600&height=900&mode=crop",
     ],
@@ -30,10 +66,39 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     isClosedTemporarily,
   } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  console.log(ratingArray);
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map((_, index) => (
+              <SvgXml
+                key={index.toString()}
+                xml={star}
+                width={20}
+                height={20}
+              />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED tEMPORARILY
+              </Text>
+            )}
+            <Spacer variant="left.large" />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <Spacer variant="left.large" />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
