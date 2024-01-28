15141a98bcc8c26295600dadb469e9275c6276a1
chamaahandsome <timboywa@gmail.com>
2024-01-28 06:17:18 -0500

    Refactor Firebase imports and authentication context

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
-import * as firebase from "firebase";
+
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// import { getAuth } from "firebase/auth";
 
 import {
   useFonts as useOswald,
@@ -28,9 +28,8 @@ const firebaseConfig = {
   appId: "1:234719356676:web:1e4b7d5c32153dae5a1251",
 };
 
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
+const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
